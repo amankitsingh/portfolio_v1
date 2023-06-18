606bd5f61c4b9371b73bbade26c88ce730e57a77
ankitsingh <1999ankits@gmail.com>
2023-06-18 23:46:10 +0530

    Intro page ready

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,18 +60,14 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Ankit Singh.</h2>;
+  const three = <h3 className="big-heading">I create amazing things.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I'm a developer from India building cool web pages, IOT applications and learning iOS app
+        development. With more than 3+ years in web developement and application developement.
+        Currently, I'm focused on building a new startup.
       </p>
     </>
   );
@@ -81,7 +77,7 @@ const Hero = () => {
       href="https://www.newline.co/courses/build-a-spotify-connected-app"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Get in Touch
     </a>
   );
 
