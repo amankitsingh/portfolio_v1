f0957eebf31740aa7fc1deebb0b2cdefab60bd38
ankitsingh <1999ankits@gmail.com>
2023-06-22 23:19:11 +0530

    image remove

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,7 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Ankit Singh',
-    description:
-      'Ankit Singh is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    description: 'Ankit Singh is a artist.',
     siteUrl: 'https://theankitsingh.in', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@amankitsingh',
