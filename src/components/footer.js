bb73f422caa7f3c01f605c1859a59891134bfb1b
ankitsingh <1999ankits@gmail.com>
2023-06-27 02:44:11 +0530

    footer change

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,22 +105,22 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://brittanychiang.com/">
-          <div>Thanks Brittany Chiang</div>
+        {/* <a href="https://brittanychiang.com/"> */}
+        <div>Thanks Brittany Chiang</div>
 
-          {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )}
-        </a>
+        {githubInfo.stars && githubInfo.forks && (
+          <div className="github-stats">
+            <span>
+              <Icon name="Star" />
+              <span>{githubInfo.stars.toLocaleString()}</span>
+            </span>
+            <span>
+              <Icon name="Fork" />
+              <span>{githubInfo.forks.toLocaleString()}</span>
+            </span>
+          </div>
+        )}
+        {/* </a> */}
       </StyledCredit>
     </StyledFooter>
   );
