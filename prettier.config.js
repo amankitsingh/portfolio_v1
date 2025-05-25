cd40d0c093cf5ddd2bcac691284446db946d2a0f
ankitsingh <1999ankits@gmail.com>
2025-05-25 13:32:30 +0530

    upgrade packages

--- prettier.config.js
+++ prettier.config.js
@@ -1,1 +1,9 @@
-module.exports = require('@upstatement/prettier-config');
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+};
