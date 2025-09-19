1ba6a0e56bd1be0e50182b3187f55d47dbcc1f39
Ankit Singh <1999ankits@gmail.com>
2025-09-19 23:55:49 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/amankitsingh',
+      url: 'https://www.instagram.com/',
     },
     {
       name: 'Twitter',
